368a6dab04a9ec33946e331f997d9ec87dee2598
Emmanuel Moreira <emmanuelmoreira@gmail.com>
2021-09-06 19:49:19 -0300

    Exemplo com DecimalFormat

--- Aulas/Aula04/Formatacao/Exemplo1.java
+++ Aulas/Aula04/Formatacao/Exemplo1.java
@@ -5,10 +5,9 @@ import java.text.DecimalFormat;
 public class Exemplo1 {
     public static void main(String[] args) {
 
-    double peso = 180.4578945465;
-    DecimalFormat df = new DecimalFormat("#.##");
-    System.out.println(df.format(peso));
+        double peso = 80.4578945465;
 
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println(df.format(peso));
     }
-
 }
